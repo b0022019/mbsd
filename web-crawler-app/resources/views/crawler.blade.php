bf4bd0722a1d2f70d6cc688d6c342d79e8b53ad0
B0021036 <b0021036@ib.yic.ac.jp>
2023-10-12 09:55:04 +0900

    crawler.blade.phpを変更した

--- web-crawler-app/resources/views/crawler.blade.php
+++ web-crawler-app/resources/views/crawler.blade.php
@@ -1,17 +1,3 @@
-{{-- <!DOCTYPE html>
-<html>
-<head>
-    <title>Web Crawler</title>
-</head>
-<body>
-    <h1>Web Crawler</h1>
-    <form method="post" action="{{ route('crawl') }}">
-        @csrf
-        <input type="text" name="url" placeholder="Enter URL">
-        <button type="submit">実行</button>
-    </form>
-</body>
-</html> --}}
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,7 +7,7 @@
     <h1>Web Crawler</h1>
     <form method="post" action="{{ route('crawl') }}">
         @csrf
-        <input type="text" name="url" placeholder="Enter URL" required>
+        <input type="text" name="url" placeholder="URLを入力してください" required>
         @error('url')
         <div class="alert alert-danger">{{ $message }}</div>
         @enderror
